b0c0245df0358fd7ae919317a0fdce8730ad2eed
Bikranshu <bikranshu.t@gmail.com>
2018-03-22 21:53:43 +0545

    migration issue fixed

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -21,17 +21,7 @@ var config = {
         publicPath: '/dist/',
     },
     plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(env)
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                pure_getters: true,
-                unsafe: true,
-                unsafe_comps: true,
-                warnings: false
-            }
-        })
+        
     ],
     module: {
         rules: [
