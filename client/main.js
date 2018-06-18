994ee522207d5a18578aeb25d1dad5b282348ed5
Gustavo Silva <gustavo.silva@aptoide.com>
2018-06-18 09:14:24 +0100

    [new] init commit

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import { blueGrey } from 'material-ui/colors';
 
