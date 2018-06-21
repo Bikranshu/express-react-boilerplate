ef5c7adcae0e7d42f8f7e7351162b22443e46125
Gustavo Silva <gustavo.silva@aptoide.com>
2018-06-21 14:37:35 +0100

    [new] fix build prod

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -2,6 +2,9 @@
 const webpack = require('webpack');
 const path = require('path');
 
+
+console.log("yooo");
+console.log(process.env.NODE_ENV);
 /*
  * so process.cwd() is used instead to determine the correct base directory
  * Read more: https://nodejs.org/api/process.html#process_process_cwd
@@ -21,6 +24,7 @@ var config = {
         publicPath: '/dist/',
     },
     plugins: [
+        new webpack.EnvironmentPlugin(['NODE_ENV']),
         new webpack.optimize.UglifyJsPlugin({
             compressor: {
                 pure_getters: true,
