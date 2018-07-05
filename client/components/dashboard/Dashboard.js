885cfd821924feabf2c7df1cc00e5b646aac2d77
krishna <bikranshu.t@gmail.com>
2018-07-05 15:30:25 +0545

    implemented material-ui grid

--- client/components/dashboard/Dashboard.js
+++ client/components/dashboard/Dashboard.js
@@ -1,19 +1,11 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {withStyles} from '@material-ui/core/styles';
 import {cyan, pink, purple, orange} from '@material-ui/core/colors';
+import Grid from '@material-ui/core/Grid';
 import {AddShoppingCart, ThumbUp, Assessment, Face} from '@material-ui/icons';
 
 import SummaryBox from './SummaryBox';
 import Product from './Product';
-
-const styles = theme => ({
-    root: theme.mixins.gutters({
-        paddingTop: 10,
-        paddingBottom: 10,
-        marginTop: theme.spacing.unit * 3,
-    }),
-});
 
 const data = {
     recentProducts: [
@@ -26,47 +18,54 @@ const data = {
 
 
 const Dashboard = props => {
+
     const classes = props.classes;
+
     return (
         <div>
             <h2 style={{paddingBottom: '15px'}}>Dashboard</h2>
 
-            <div className="row">
-                <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
+            <Grid container spacing={24}>
+                <Grid item xs>
                     <SummaryBox Icon={AddShoppingCart}
                                 color={pink[600]}
                                 title="Total Profit"
                                 value="1500k"
                     />
-                </div>
-                <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
+                </Grid>
+
+               <Grid item xs>
                     <SummaryBox Icon={ThumbUp}
                                 color={cyan[600]}
                                 title="Likes"
                                 value="4231"
                     />
-                </div>
-                <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
+                </Grid>
 
+               <Grid item xs>
                     <SummaryBox Icon={Assessment}
                                 color={purple[600]}
                                 title="Sales"
                                 value="460"
                     />
-                </div>
-                <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
+                </Grid>
+
+                <Grid item xs>
                     <SummaryBox Icon={Face}
                                 color={orange[600]}
                                 title="New Members"
                                 value="248"
                     />
-                </div>
-            </div>
-            <div className="row" style={{paddingTop: '10px'}}>
-                <div className="col-xs-12 col-sm-12">
+                </Grid>
+
+            </Grid>
+
+             <Grid container spacing={24}>
+               <Grid item xs>
                     <Product data={data.recentProducts}/>
-                </div>
-            </div>
+                </Grid>
+            </Grid>
+
         </div>
     )
 };
@@ -75,4 +74,4 @@ Dashboard.propTypes = {
     classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(Dashboard)
+export default Dashboard
