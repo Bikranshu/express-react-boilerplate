4b04f33617fc880ce8bdafcc15030e4abb9b0a8e
Krishna Timilsina <krishna.timilsina@citytech.global>
2019-03-05 23:52:47 +0545

    ....

--- client/main.js
+++ client/main.js
@@ -5,7 +5,7 @@ import {ConnectedRouter} from 'connected-react-router';
 import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 import {blueGrey} from '@material-ui/core/colors';
 
-import store, { history } from './store/configureStore';
+import store, {history} from './store/configureStore';
 import {verifyToken} from './services/tokenService';
 import App from './containers/app/AppContainer';
 
@@ -22,27 +22,13 @@ const theme = createMuiTheme({
 // Used to log in if token is valid
 store.dispatch(verifyToken());
 
-// const renderApp = (Component) => {
-    ReactDOM.render(
-        <MuiThemeProvider theme={theme}>
-            <Provider store={store}>
-                <ConnectedRouter history={history}>
-                <App />
-                </ConnectedRouter>
-            </Provider>
-        </MuiThemeProvider>,
-        mountNode
-    );
-// };
-
-// renderApp(App);
-
-// // Webpack Hot Module Replacement API
-// if (module.hot) {
-//     module.hot.accept('./containers/app/AppContainer', () => {
-//         // if you are using harmony modules ({modules:false})
-//         renderApp(App);
-//         // in all other cases - re-require App manually
-//         renderApp(require('./containers/app/AppContainer'));
-//     });
-// }
+ReactDOM.render(
+    <MuiThemeProvider theme={theme}>
+        <Provider store={store}>
+            <ConnectedRouter history={history}>
+                <App/>
+            </ConnectedRouter>
+        </Provider>
+    </MuiThemeProvider>,
+    mountNode
+);
