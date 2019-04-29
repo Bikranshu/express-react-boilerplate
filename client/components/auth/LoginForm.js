12c9969ed124b826c3ab6c90dc056af5f66edc1a
Krishna Timilsina <krishna.timilsina@citytech.global>
2019-04-29 23:09:08 +0545

    upgrade version

--- client/components/auth/LoginForm.js
+++ client/components/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import {Field, reduxForm} from 'redux-form'
