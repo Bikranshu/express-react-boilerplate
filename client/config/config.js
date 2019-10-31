4045d6b8f60cd99468031b8e41de8b0e12fcefe3
Krishna Timilsina <krishna.timilsina@citytech.global>
2019-10-31 14:13:27 +0545

    code refactored

--- client/config/config.js
+++ client/config/config.js
@@ -5,3 +5,4 @@ export const APP_PORT = process.env.APP_PORT || 3000;
 export const HOST = `${APP_HOST}:${APP_PORT}/`;
 
 export const API_URL = `http://${HOST}${apiPath}`;
+export const JWT_TOKEN = 'token';
