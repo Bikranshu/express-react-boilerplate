02dde42b14a23ceb047e3b0708ed96c9fd7af99b
Krishna Timilsina <krishna.timilsina@citytech.global>
2020-03-31 16:22:47 +0545

    code refactored

--- client/main.js
+++ client/main.js
@@ -1,34 +1,36 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {ConnectedRouter} from 'connected-react-router';
-import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
-import {blueGrey} from '@material-ui/core/colors';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { blueGrey } from '@material-ui/core/colors';
 
-import store, {history} from './store/configureStore';
-import {verifyToken} from './services/tokenService';
+import store, { history } from './store/configureStore';
+import { verifyToken } from './services/tokenService';
 import App from './containers/app/AppContainer';
 
 const mountNode = document.getElementById('root');
 const theme = createMuiTheme({
-    typography: {
-        useNextVariants: true,
-    },
-    palette: {
-        primary: blueGrey
-    }
+  typography: {
+    useNextVariants: true,
+  },
+  palette: {
+    primary: blueGrey,
+  },
 });
 
 // Used to log in if token is valid
 store.dispatch(verifyToken());
 
 ReactDOM.render(
+  <Suspense fallback={<div>Error! Please refresh the page</div>}>
     <MuiThemeProvider theme={theme}>
-        <Provider store={store}>
-            <ConnectedRouter history={history}>
-                <App/>
-            </ConnectedRouter>
-        </Provider>
-    </MuiThemeProvider>,
-    mountNode
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <App />
+        </ConnectedRouter>
+      </Provider>
+    </MuiThemeProvider>
+  </Suspense>,
+  mountNode
 );
--- client/services/authService.js
+++ client/services/authService.js
@@ -1,36 +1,30 @@
 import axios from 'axios';
+import { push } from 'connected-react-router';
 
-import {loginSuccess, loginFailure, logoutSuccess} from '../actions/authAction';
-import history from '../utils/history';
-import {API_URL, JWT_TOKEN} from '../config/config';
-import {setLocalStorage, clearLocalStorage} from '../utils/storageUtil';
+import { loginSuccess, loginFailure, logoutSuccess } from '../actions/authAction';
+import { API_URL, JWT_TOKEN } from '../config/config';
+import { setLocalStorage, clearLocalStorage } from '../utils/storageUtil';
 
-export const login = ({email, password}) => {
-
-    return dispatch => {
-        axios.post(API_URL + 'auth/login', {email, password}).then((response) => {
-
-            dispatch(loginSuccess(response.data.token));
-
-            setLocalStorage(JWT_TOKEN, response.data.token);
-
-            history.push('/dashboard');
-        })
-            .catch((error) => {
-                dispatch(loginFailure(error.response.data));
-            });
-    };
+export const login = ({ email, password }) => {
+  return (dispatch) => {
+    axios
+      .post(API_URL + 'auth/login', { email, password })
+      .then((response) => {
+        dispatch(loginSuccess(response.data.token));
+        setLocalStorage(JWT_TOKEN, response.data.token);
+        dispatch(push('/dashboard'));
+      })
+      .catch((error) => {
+        dispatch(loginFailure(error.response.data));
+      });
+  };
 };
 
 export const logout = () => {
-    return dispatch => {
-
-        clearLocalStorage(JWT_TOKEN);
-
-        dispatch(logoutSuccess());
-
-        history.push('/');
-
-        return false;
-    };
+  return (dispatch) => {
+    clearLocalStorage(JWT_TOKEN);
+    dispatch(logoutSuccess());
+    dispatch(push('/'));
+    return false;
+  };
 };
