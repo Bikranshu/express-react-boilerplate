f08b89ed4d51325eeea7576f99b079782ddb940d
Krishna Timilsina <krishna.timilsina@citytech.global>
2020-03-31 15:44:19 +0545

    improvements

--- client/components/auth/LoginForm.js
+++ client/components/auth/LoginForm.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
-import {Field, reduxForm} from 'redux-form'
-import {withStyles} from '@material-ui/core/styles';
-import {Card, CardHeader, CardContent} from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
+import { withStyles } from '@material-ui/core/styles';
+import { Card, CardHeader, CardContent } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 
 // Import custom components
@@ -11,102 +11,84 @@ import renderText from '../common/form/renderText';
 import CustomizedSnackbar from '../common/snakebar/CustomizedSnackbar';
 
 const styles = {
-    root: {
-        minWidth: 320,
-        maxWidth: 400,
-        height: 'auto',
-        position: 'absolute',
-        top: '15%',
-        left: 0,
-        right: 0,
-        margin: 'auto'
-    },
-    card: {
-        padding: 20,
-        overflow: 'auto'
-    },
-    cardHeader: {
-        textAlign: 'center'
-    },
-    btnDiv: {
-        textAlign: 'center'
-    },
-    btn: {
-        marginTop: 21,
-    }
+  root: {
+    minWidth: 320,
+    maxWidth: 400,
+    height: 'auto',
+    position: 'absolute',
+    top: '15%',
+    left: 0,
+    right: 0,
+    margin: 'auto',
+  },
+  card: {
+    padding: 20,
+    overflow: 'auto',
+  },
+  cardHeader: {
+    textAlign: 'center',
+  },
+  btnDiv: {
+    textAlign: 'center',
+  },
+  btn: {
+    marginTop: 21,
+  },
 };
 
-const LoginForm = props => {
+const LoginForm = (props) => {
+  const { handleSubmit, onSubmit, classes, errorMessage } = props;
 
-    const {handleSubmit, onSubmit, classes, errorMessage} = props;
-
-    return (
-        <div className={classes.root}>
-            <Card className={classes.card}>
-                <CardHeader
-                    className={classes.cardHeader}
-                    title="Login"
-                />
-                {errorMessage  &&
-                <CustomizedSnackbar
-                    variant="error"
-                    className={classes.margin}
-                    message={ errorMessage }
-                />}
-                <CardContent>
-                    <form method="post" onSubmit={handleSubmit(onSubmit)}>
-                        <Field
-                            type="text"
-                            name="email"
-                            component={renderText}
-                            label="Username"
-                        />
-                        <br />
-                        <Field
-                            type="password"
-                            name="password"
-                            component={renderText}
-                            label="Password"
-
-                        />
-                        <br />
-                        <div className={classes.btnDiv}>
-                            <Button className={classes.btn} type="submit" variant="contained" color="primary">Login</Button>
-                            <p>Don't have an account? <Link to={'/signup'}>Create one</Link>.</p>
-                        </div>
-                    </form>
-                </CardContent>
-
-            </Card>
-        </div>
-    )
+  return (
+    <div className={classes.root}>
+      <Card className={classes.card}>
+        <CardHeader className={classes.cardHeader} title="Login" />
+        {errorMessage && (
+          <CustomizedSnackbar variant="error" className={classes.margin} message={errorMessage} />
+        )}
+        <CardContent>
+          <form method="post" onSubmit={handleSubmit(onSubmit)}>
+            <Field type="text" name="email" component={renderText} label="Username" />
+            <br />
+            <Field type="password" name="password" component={renderText} label="Password" />
+            <br />
+            <div className={classes.btnDiv}>
+              <Button className={classes.btn} type="submit" variant="contained" color="primary">
+                Login
+              </Button>
+              <p>
+                Don't have an account? <Link to={'/signup'}>Create one</Link>.
+              </p>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
-const validateLogin = values => {
-    const errors = {};
+const validateLogin = (values) => {
+  const errors = {};
 
-    const requiredFields = [
-        'email',
-        'password'
-    ];
-    requiredFields.forEach(field => {
-        if (!values[field]) {
-            errors[field] = '(The ' + field + ' field is required.)';
-        }
-    });
-
-    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = '(Invalid email address.)';
+  const requiredFields = ['email', 'password'];
+  requiredFields.forEach((field) => {
+    if (!values[field]) {
+      errors[field] = '(The ' + field + ' field is required.)';
     }
-    return errors
+  });
+
+  if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = '(Invalid email address.)';
+  }
+  return errors;
 };
 
 LoginForm.propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-    classes: PropTypes.object.isRequired
+  onSubmit: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
 export default reduxForm({
-    form: 'LoginForm', // a unique identifier for this form
-    validate: validateLogin // ←Callback function for client-side validation
-})(withStyles(styles)(LoginForm))
+  form: 'LoginForm', // a unique identifier for this form
+  validate: validateLogin, // ←Callback function for client-side validation
+})(withStyles(styles)(LoginForm));
--- client/components/common/form/renderText.js
+++ client/components/common/form/renderText.js
@@ -1,25 +1,23 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 
-const renderText = ({input, label, type, meta: {touched, error, invalid}}) => (
-
-    <TextField
-        type={type}
-        label={label}
-        error={touched && invalid}
-        helperText={touched && error}
-        margin="normal"
-        fullWidth={true}
-        {...input}
-    />
-
+const renderText = ({ input, label, type, meta: { touched, error, invalid } }) => (
+  <TextField
+    type={type}
+    label={label}
+    error={touched && invalid}
+    helperText={touched && error}
+    margin="normal"
+    fullWidth={true}
+    {...input}
+  />
 );
 
 renderText.propTypes = {
-    input: PropTypes.object.isRequired,
-    label: PropTypes.string.isRequired,
-    meta: PropTypes.object
+  input: PropTypes.object.isRequired,
+  label: PropTypes.string.isRequired,
+  meta: PropTypes.object,
 };
 
 export default renderText;
